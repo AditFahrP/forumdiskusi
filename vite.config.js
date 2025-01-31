318e7a4ebe613bdbb48c2d8bbc069d88c3bc1b7a
adithia <aditpahrudin7@gmail.com>
2025-01-31 18:30:51 +0700

    Update
    Update package

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
+    server: {
+        host: "0.0.0.0", // Railway butuh ini
+        port: 5173, // Default port Vite
+        strictPort: true,
+        hmr: {
+            host: process.env.VITE_APP_URL || "your-railway-app-url",
+            protocol: "wss",
+        },
+    },
+    build: {
+        outDir: "public/build", // Pastikan file build masuk ke public
+        emptyOutDir: true,
+    },
 });
