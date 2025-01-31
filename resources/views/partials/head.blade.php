4b8e27db15161ee6f0fba5ed1596251f78634516
adithia <aditpahrudin7@gmail.com>
2025-01-31 19:45:33 +0700

    Update head.blade.php

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Diskusi</title>
-
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
 @vite(['resources/scss/app.scss', 'resources/js/app.js'])
