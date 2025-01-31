f5b41c8d8d6af01f39844dd6c1c3d624f6232734
adithia <aditpahrudin7@gmail.com>
2025-02-01 06:18:12 +0700

    Update tambah vercel.json

--- vite.config.js
+++ vite.config.js
@@ -4,21 +4,8 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: ["resources/scss/app.scss", "resources/js/app.js"],
             refresh: true,
         }),
     ],
-    server: {
-        host: "0.0.0.0", // Railway butuh ini
-        port: 5173, // Default port Vite
-        strictPort: true,
-        hmr: {
-            host: process.env.VITE_APP_URL || "your-railway-app-url",
-            protocol: "wss",
-        },
-    },
-    build: {
-        outDir: "public/build", // Pastikan file build masuk ke public
-        emptyOutDir: true,
-    },
 });
