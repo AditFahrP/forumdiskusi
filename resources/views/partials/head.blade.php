d69ce0a7c506c183f54799daeda8d0e3b4961252
adithia <aditpahrudin7@gmail.com>
2025-02-01 07:39:07 +0700

    Update baru

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,5 +4,4 @@
 <title>Diskusi</title>
 <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
-<link href="{{ asset('build/assets/app-e4bd7279.css') }}" rel="stylesheet">
 @vite(['resources/scss/app.scss', 'resources/js/app.js'])
